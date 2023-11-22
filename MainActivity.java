8a9b3da47d97f6ae8c62fbc2ac37d5112375c846
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2023-11-22 23:27:33 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button sendButton = findViewById(R.id.sendButton);
+        // Implement the file sending logic when the button is clicked
+    }
+}
